4ead7daf131362149998c5dab293f9c925d71da3
reubensoap <34084097+reubensoap@users.noreply.github.com>
2020-02-28 01:48:22 -0600

    Add files via upload

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -1,5 +1,84 @@
 package com.meritamerica.assignment1;
+import java.text.DecimalFormat;
 
 public class AccountHolder {
-	
+
+    // Variables of Class
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String ssn;
+	public SavingsAccount savingsAccount = new SavingsAccount();
+	public CheckingAccount checkingAccount = new CheckingAccount();
+
+	// Constructors
+    public AccountHolder() {
+        this.firstName = "John";
+        this.middleName = "M";
+        this.lastName = "Doe";
+        this.ssn = "n/a";
+    }
+
+    public AccountHolder(String fn, String mn, String ln, String sn, double checkB, double savingB){
+        this.firstName = fn;
+        this.middleName = mn;
+        this.lastName = ln;
+        this.ssn = sn;
+        this.checkingAccount.deposit(checkB);
+        this.savingsAccount.deposit(savingB);
+    }
+
+    // Setters and Getters
+    public void setFirstName(String fn){
+        this.firstName = fn;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public void setMiddleName(String mn){
+        this.middleName = mn;
+    }
+
+    public String getMiddleName(){
+        return this.middleName;
+    }
+
+    public void setLastName(String ln){
+        this.lastName = ln;
+    }
+
+    public String getLastname(){
+        return this.lastName;
+    }
+
+    public void setSSN(String sn){
+        this.ssn = sn;
+    }
+
+    public String getSSN(){
+        return this.ssn;
+    }
+
+    // Methods
+    public CheckingAccount getCheckingAccount(){
+        return this.checkingAccount;
+    }
+
+    public SavingsAccount getSavingsAccount(){
+        return this.savingsAccount;
+    }
+
+    public String toString(){
+        DecimalFormat format = new DecimalFormat("##.00");
+        return "Name: " + this.firstName + " " + this.middleName + " " + this.lastName + "\n"
+                + "SSN: " + this.ssn
+                + this.checkingAccount
+                + this.savingsAccount + "\n";
+    }
+
+
+
+
 }
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -1,5 +1,68 @@
 package com.meritamerica.assignment1;
+import java.text.DecimalFormat;
 
 public class SavingsAccount {
+
+    // Variables
+    private double balance;
+    private static double interest = 0.01;
+
+    //Constructors
+    public SavingsAccount(){
+        this.balance = 0.0;
+    }
+
+    public SavingsAccount(double savingB){
+        this.balance = savingB;
+    }
+
+    //Getters and Setters
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public double getInterestRate(){
+        return interest;
+    }
+
+    //Methods
+    public double futureValue(int years){
+        double pv = this.getBalance();
+        double fv = pv * (Math.pow((1 + this.getInterestRate()), years));
+        return fv;
+    }
+
+    public boolean withdraw(double amount){
+        if (amount < 0){
+            System.out.println("Unable to withdraw a negative amount. Try again.");
+            return false;
+        } else {
+            if((this.getBalance() - amount) < 0){
+                System.out.println("Not enough funds in the account.");
+                return false;
+            } else {
+                double newB = this.getBalance() - amount;
+                this.balance = newB;
+                return true;
+            }
+        }
+    }
+
+    public boolean deposit(double amount){
+        if(amount <= 0){
+            System.out.println("Unable to deposit negative funds. Try again.");
+            return false;
+        } else {
+            this.balance = this.getBalance() + amount;
+            return true;
+        }
+    }
+
+    public String toString(){
+        DecimalFormat format = new DecimalFormat("##.00");
+        return "Saving Account Balance: $" + format.format(this.getBalance()) + "\n"
+                + "Saving Account Interest Rate: " + this.getInterestRate() + "\n"
+                + "Saving Account Balance in 3 years: " + format.format(this.futureValue(3)) + "\n";
+    }
 	
 }
